1be479cbb3749452580b3f638fc95f64d957cdf1
Sam Allen <Sam.Allen@beveragebot.com>
2019-12-08 23:38:22 -0800

    initial commit

---
+++ src/main/java/com/bevbot/order/drink/api/gateway/OrderDrinkApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bevbot.order.drink.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderDrinkApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderDrinkApiGatewayApplication.class, args);
+    }
+
+}
